f70bb8ba57526c3e9631f1a001cdfbef99160e00
Vipul <vipul.sharma20@gmail.com>
2018-01-31 16:34:27 +0530

    check multiple regions for color

--- shatsu.py
+++ shatsu.py
@@ -17,7 +17,7 @@ class Shatsu(object):
         Z = np.float32(Z)
         # define criteria, number of clusters(K) and apply kmeans()
         criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
-        K = 8
+        K = 5
 
         ret, label, center = cv2.kmeans(Z, K, None, criteria, 10,
                                         cv2.KMEANS_RANDOM_CENTERS)
@@ -43,6 +43,7 @@ class Shatsu(object):
 
         faces = self.face_cascade.detectMultiScale(gray, 1.3, 5)
         detected = False
+        sensitivity = False
 
         for (x, y, w, h) in faces:
             detected = True
@@ -54,48 +55,54 @@ class Shatsu(object):
             # Shirt coordinates
             offset_y_bottom = 70
             offset_x_left = 30
-            shirt_x = x + (w // 3) - offset_x_left
+            offset_x_right = 30
             shirt_y = y + h + offset_y_bottom
             shirt_w = w // 3
             shirt_h = w // 3
 
-            print(shirt_x, shirt_y, shirt_w, shirt_h)
-
-            crop_img = blur[shirt_y:shirt_y+shirt_h, shirt_x:shirt_x+shirt_w]
             try:
-                crop_img = self.compress(crop_img)
+                # shirt region 1 (left)
+                shirt_x = x + (w // 3) - offset_x_left
+                sensitivity_1 = self.shirt_region(blur, shirt_x, shirt_y, shirt_h, shirt_w, '1')
+                cv2.rectangle(img, (shirt_x, shirt_y),
+                              (shirt_x+shirt_w, shirt_y+shirt_h), (255, 0, 0), 2)
+
+                # shirt region 2 (right)
+                shirt_x = x + (w // 3) + offset_x_right
+                sensitivity_2 = self.shirt_region(blur, shirt_x, shirt_y, shirt_h, shirt_w, '2')
+                cv2.rectangle(img, (shirt_x, shirt_y),
+                              (shirt_x+shirt_w, shirt_y+shirt_h), (255, 0, 0), 2)
+
+                sensitivity = sensitivity_1 or sensitivity_2
+                break
             except cv2.error:
                 logging.debug(f"Error in K means {self.img_path[3:]}")
                 continue
-
-            try:
-                avg_color_per_row = np.average(crop_img, axis=0)
-                avg_color = np.average(avg_color_per_row, axis=0)
             except ZeroDivisionError:
                 logging.debug(f"Division by Zero {self.img_path[3:]}")
                 continue
 
-            font = cv2.FONT_HERSHEY_SIMPLEX
-            r, g, b = map(lambda x: int(x) if not math.isnan(x) else x,
-                          [avg_color[2], avg_color[1], avg_color[0]])
-            text = f'{r}, {g}, {b}'
-            # cv2.putText(img, check_uniform(r, g, b), (100, 100),
-            #            font, 1, (0, 0, 255), 2, cv2.LINE_AA)
-            cv2.putText(img, 'con: ' + self.color_detect(crop_img)+'%' + self.check_uniform(r,g,b), (50, 50),
-                        font, 1, (0, 0, 255), 2, cv2.LINE_AA)
-            print(avg_color)
-
-            # bounding rectangle for shirt
-            cv2.rectangle(img, (shirt_x, shirt_y),
-                          (shirt_x+shirt_w, shirt_y+shirt_h), (255, 0, 0), 2)
-            break
-
         if detected:
-            cv2.imwrite('new/shirt_'+self.img_path[3:], img)
-            cv2.imwrite('crop/shirt_'+self.img_path[3:], crop_img)
+            self.img_path = self.img_path.split('/')[-1]
+
+            cv2.imwrite(('true_positive/' if sensitivity else 'false_negative/')+self.img_path, img)
         else:
             # cv2.imwrite('noface/shirt_'+f[3:], img)
             logging.debug(f"No face detected in {self.img_path[3:]}")
+
+    def shirt_region(self, blur, shirt_x, shirt_y, shirt_h, shirt_w, name):
+        crop_img = blur[shirt_y:shirt_y+shirt_h, shirt_x:shirt_x+shirt_w]
+        crop_img = self.compress(crop_img)
+
+        avg_color_per_row = np.average(crop_img, axis=0)
+        avg_color = np.average(avg_color_per_row, axis=0)
+
+        r, g, b = map(lambda x: int(x) if not math.isnan(x) else x,
+                      [avg_color[2], avg_color[1], avg_color[0]])
+
+        cv2.imwrite('crop/'+name+self.img_path.split('/')[-1], crop_img)
+
+        return self.check_uniform(r, g, b)
 
     def color_detect(self, img):
         confidence = 0
@@ -109,10 +116,8 @@ class Shatsu(object):
     def check_uniform(self, *rgb):
         r, g, b = rgb
         diff = abs(g-b)
-        if diff <= 20 and max(g, b) - 20 >= r:
-            return " in uniform"
-        else:
-            return " not in uniform"
+
+        return True if diff <= 20 and max(g, b) - 20 >= r else False
 
 
 if __name__ == '__main__':
